c31a28931e7d9dacdb534ede706216ff05d47f16
ramadhan07 <35648242+ramadhan07@users.noreply.github.com>
2018-04-02 23:21:13 -0700

    membuat atribut dan behavior

--- praktek02/src/praktek02/penjualansepatu.java
+++ praktek02/src/praktek02/penjualansepatu.java
@@ -1,5 +1,13 @@
 package praktek02;
 
 public class penjualansepatu {
+    int harga_sepatu;
+    int jumlah_barang;
     
+    void cetakInfo(){
+        System.out.println("============================");
+        System.out.println("harga sepatu :"+harga_sepatu);
+        System.out.println("harga sepatu :"+jumlah_barang);
+        System.out.println("============================");
+    }
 }
