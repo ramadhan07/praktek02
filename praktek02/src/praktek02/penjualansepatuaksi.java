30d3f0fa4393410876f66f9f5f5e9ae8d9cf7247
ramadhan07 <35648242+ramadhan07@users.noreply.github.com>
2018-04-02 23:29:48 -0700

    mengisi nilai atribute

--- praktek02/src/praktek02/penjualansepatuaksi.java
+++ praktek02/src/praktek02/penjualansepatuaksi.java
@@ -4,5 +4,9 @@ public class penjualansepatuaksi {
     public static void main(String[] args) {
         penjualansepatu r1 = new penjualansepatu();
         
+        r1.harga_sepatu = 350000;
+        r1.jumlah_barang = 4;
+        
+        
     }
 }
